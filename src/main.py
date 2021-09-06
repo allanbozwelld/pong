f47e7f2e689ca7f15631258dcc511c81cc712aa1
allanbozwelld <90142863+allanbozwelld@users.noreply.github.com>
2021-09-06 14:19:30 -0500

    Add main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,17 @@
+from controller.controllers import Controller
+from event.events import EventManager
+from model.models import Model
+from view.views import View
+
+
+def start():
+    event_manager = EventManager()
+    model = Model(event_manager=event_manager)
+    controller = Controller(model=model, event_manager=event_manager)
+    view = View(model=model, event_manager=event_manager)
+
+    model.run()
+
+
 if __name__ == "__main__":
-    pass
+    start()
