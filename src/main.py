aca9a65ed89619f53f12dcbb99d1547307fe9d2d
allanbozwelld <90142863+allanbozwelld@users.noreply.github.com>
2021-09-06 19:25:16 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@ from view.views import View
 
 
 def start():
-    event_manager = EventManager()
-    model = Model(event_manager=event_manager)
-    controller = Controller(model=model, event_manager=event_manager)
-    view = View(model=model, event_manager=event_manager)
+    manager: EventManager = EventManager()
+    model: Model = Model(manager=manager)
+    controller: Controller = Controller(model=model, manager=manager)
+    view: View = View(model=model, manager=manager)
 
     model.run()
 
